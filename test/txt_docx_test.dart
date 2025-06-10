d7f011f7371f5a0ee96fd1906127f41991c00965
lindeer <le.chang118@gmail.com>
2025-06-11 07:17:26 +0800

    add testing

--- test/txt_docx_test.dart
+++ test/txt_docx_test.dart
@@ -1,8 +1,28 @@
+import 'dart:io';
+
 import 'package:txt_docx/txt_docx.dart';
 import 'package:test/test.dart';
 
-void main() {
-  group('A group of tests', () {
+const _sample = """
+This is the first paragraph of my document.
+Here is a second paragraph. It contains some more text.
+And finally, a third one. This demonstrates simple text storage.
+Special characters like é, à, ç should also work fine.
 
+You can open this .docx file in Microsoft Word or any compatible viewer.
+This is paragraph number 0, demonstrating handling of many paragraphs.
+""";
+void main() {
+  test('basic read and write', () async {
+    final lines = _sample.split('\n');
+    final docx = 'tmp.docx';
+    final writer = DocxWriter();
+    await writer.writeLines(lines, docx);
+
+    final rf = File(docx);
+    final decoder = DocxDecoder(rf.lengthSync());
+    final text = await rf.openRead().transform(decoder).join('');
+    expect(text, '$_sample\n');
+    rf.deleteSync();
   });
 }
